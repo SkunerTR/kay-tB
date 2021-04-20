4641d7ab50e5e9d316edd42b4f95206f9c065388
SkunerTR <75324763+SkunerTR@users.noreply.github.com>
2021-04-20 21:12:19 +0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  "token": "Botunuzun tokeni buraya gelecek!",
+  "geliştiriciler": ["377445761312489472"], 
+  "prefix": "w.",
+
+ "sunucuid": "792032896642842675",
+ "kayitKanal": "824921552419815454",
+ "genelChat": "833591540878147594",
+
+ "kizRol": "813800240079044638",
+ "vip": "792032897066074142",
+ "erkekRol": "813800246492004392",
+ "kayitsiz": "814501087671091270",
+ "teyitci": "814420454705594369"
+};
